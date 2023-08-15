d3813324dadea0fa74c215173e143c4e03be36d4
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-08-15 21:49:05 +0530

    basic Big O example

---
+++ Big O/basic-example.ts
@@ -0,0 +1,29 @@
+const pref = require("perf_hooks");
+
+const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
+const large = new Array(100000).fill("nemo");
+
+function findNemo(array: string[]) {
+  let t0 = pref.performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Found NEMO!");
+    }
+  }
+  let t1 = pref.performance.now();
+  console.log(`Call to find Nemo took ${t1 - t0} milliseconds`);
+}
+
+findNemo(large);
