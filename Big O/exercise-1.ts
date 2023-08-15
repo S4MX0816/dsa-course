87d463b401c12dd8b0b281d63ae7e34080dfa8cc
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-08-15 22:46:32 +0530

    exercise 1 solution

--- Big O/exercise-1.ts
+++ Big O/exercise-1.ts
@@ -1,14 +1,26 @@
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function funChallenge(input: string[]) {
-  let a = 10;
-  a = 50 + 3;
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
 
+  // O(n)
   for (let i = 0; i < input.length; i++) {
-    anotherFunction();
-    let stranger = true;
-    a++;
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
   }
-  return a;
+  return a; // O(1)
 }
 
 function anotherFunction() {}
+
+funChallenge(["a", "b", "c"]);
+
+/////////////////////////////////////////
+// Time Complexity
+//
+// O(1 + 1 + 1 + n + n + n + n)
+// O(3 + 4n) --> O(n)
+//
+// End of Time Complexity
+/////////////////////////////////////////
